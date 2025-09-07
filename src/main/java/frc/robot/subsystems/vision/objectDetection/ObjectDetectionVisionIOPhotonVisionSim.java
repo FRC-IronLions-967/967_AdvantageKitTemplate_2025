52591d427fb808bedbd496859e12c1f3774eb9e8
331282 <148924183+331282@users.noreply.github.com>
2025-09-07 16:02:19 -0500

    Idk but looks like I forgot to push in June

--- src/main/java/frc/robot/subsystems/vision/objectDetection/ObjectDetectionVisionIOPhotonVisionSim.java
+++ src/main/java/frc/robot/subsystems/vision/objectDetection/ObjectDetectionVisionIOPhotonVisionSim.java
@@ -6,7 +6,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import java.util.function.Supplier;
-import org.littletonrobotics.junction.Logger;
 import org.photonvision.estimation.TargetModel;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
@@ -17,7 +16,11 @@ public class ObjectDetectionVisionIOPhotonVisionSim extends ObjectDetectionVisio
   private final VisionSystemSim simSystem;
   private final PhotonCameraSim cameraSim;
   private final Supplier<Pose2d> poseSupplier;
-  private final VisionTargetSim[] objectTargets;
+  private final TargetModel coral;
+  private Pose3d coralPose;
+  private VisionTargetSim coralTarget;
+
+  private SimCameraProperties cameraProp;
 
   public ObjectDetectionVisionIOPhotonVisionSim(
       String name, Transform3d robotToCamera, Supplier<Pose2d> poseSupplier) {
@@ -27,38 +30,30 @@ public class ObjectDetectionVisionIOPhotonVisionSim extends ObjectDetectionVisio
     // Sim system *without* AprilTags
     simSystem = new VisionSystemSim("objectDetectionOnly");
 
-    // Define objects to detect (could be coral, cubes, etc.)
-    objectTargets =
-        new VisionTargetSim[] {
-          new VisionTargetSim(new Pose3d(1, 5.0, 0.2, new Rotation3d()), new TargetModel(0.3, 0.3)),
-          new VisionTargetSim(new Pose3d(1, 1.0, 0.2, new Rotation3d()), new TargetModel(0.3, 0.3)),
-          new VisionTargetSim(new Pose3d(-2.0, 0.0, 0.0, new Rotation3d()), new TargetModel(0.3, 0.3)) // 2 meters behind robot
-        };
-    simSystem.addVisionTargets("DetectedObjects", objectTargets);
+    // create coral
+    coral = new TargetModel(0.5, 0.25);
+    coralPose = new Pose3d(1, 1, 1, new Rotation3d());
+    coralTarget = new VisionTargetSim(coralPose, coral);
+    simSystem.addVisionTargets(coralTarget);
 
-    // Set up camera
-    var props = new SimCameraProperties();
-    props.setFPS(30);
-    props.setCalibration(640, 480, new Rotation2d(Math.PI / 3));
+    // The simulated camera properties
+    cameraProp = new SimCameraProperties();
+    // A 640 x 480 camera with a 100 degree diagonal FOV.
+    cameraProp.setCalibration(640, 480, Rotation2d.fromDegrees(100));
+    // Approximate detection noise with average and standard deviation error in pixels.
+    cameraProp.setCalibError(0.25, 0.08);
+    // Set the camera image capture framerate (Note: this is limited by robot loop rate).
+    cameraProp.setFPS(20);
+    // The average and standard deviation in milliseconds of image data latency.
+    cameraProp.setAvgLatencyMs(35);
+    cameraProp.setLatencyStdDevMs(5);
 
-    cameraSim = new PhotonCameraSim(super.camera, props, null);
-    cameraSim.setMinTargetAreaPixels(0); // or something small
-    cameraSim.setMaxSightRange(10.0); // meters
+    cameraSim = new PhotonCameraSim(super.camera, cameraProp);
     simSystem.addCamera(cameraSim, robotToCamera);
   }
 
   @Override
   public void updateInputs(VisionIOInputs inputs) {
-    Pose2d[] objectPoses = new Pose2d[objectTargets.length];
-    for (int i = 0; i < objectTargets.length; i++) {
-      objectPoses[i] =
-          new Pose2d(
-              objectTargets[i].getPose().getX(),
-              objectTargets[i].getPose().getY(),
-              new Rotation2d());
-    }
-    Logger.recordOutput("Vision/Coral", objectPoses);
-
     simSystem.update(poseSupplier.get());
     super.updateInputs(inputs);
   }
